2b9a3945a8f32019c5ec30eb7bf37298c412a84f
unknown <webxite23@gmail.com>
2020-11-21 22:41:57 +0200

    i added thsong datas from chillhop

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
+//importing Components
+//importing data
+import Chillhop from "./Data"
+//importing data
+
 import Song from "./components/Song";
 import Player from "./components/Player";
+//importing  styles
+import "./styles/App.scss";
 function App() {
+  //adding state
+  const [songs, setSongs] = useState(Chillhop());
   return (
-    <div className="App">
-      <h1>Music player</h1>
+    <div className="">
       <Song />
       <Player />
     </div>
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Player = () => {
+import {
+  faPlay,
+  faAngleLeft,
+  faAngleRight,
+} from "@fortawesome/free-solid-svg-icons";
+
+
+const Player = (props) => {
+  const song1 = props.song;
+  const [play, setPlay] = useState([]);
+  const [play0, setPlay0] = useState(song1);
+  const play1 = () => {
+
+    return(
+setPlay()
+
+
+    )
+  };
+
   return (
     <div className="player">
       <div className="time-control">
@@ -8,7 +28,22 @@ const Player = () => {
         <input type="range" />
         <p>End time</p>
       </div>
-      <div className="play-control"></div>
+      <div className="play-control">
+        <FontAwesomeIcon className="skip-back" size="3x" icon={faAngleLeft} />
+        <FontAwesomeIcon
+          className="play"
+          size="3x"
+          icon={faPlay}
+          onClick={play1}
+        />
+
+        <FontAwesomeIcon
+          className="skip-forward"
+          size="3x"
+          icon={faAngleRight}
+        />
+      </div>
+
     </div>
   );
 };
